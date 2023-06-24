9b7e9dadabe6127c992a1f04aacfc9090446e46c
cody8764 <111713735+cody8764@users.noreply.github.com>
2023-06-24 11:35:02 -0400

    hhhhhhhhhhhh

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -9,3 +9,9 @@ function addUser() {
     window.location = "quiz_game_page.html";
     
 }
+
+function send() {
+number1 = document.getElementById("number1").value;
+number2 = document.getElementById("number2").value; 
+actual_answer = parseInt(number1) * parseInt(number2);
+}
