83495b6bdf9fa550026df7cb064c8e5ab897116e
cody8764 <111713735+cody8764@users.noreply.github.com>
2023-06-30 09:32:38 -0400

    Sixth Commit

--- quiz_game_login.js
+++ quiz_game_login.js
@@ -1,17 +1,83 @@
-function addUser() {
-    
-    player1_name = document.getElementById("player1_name_input").value;
-    player2_name = document.getElementById("player2_name_input").value;
-    
-    localStorage.setItem("player1_name", player1_name);
-    localStorage.setItem("player2_name", player2_name);
-    
-    window.location = "quiz_game_page.html";
-    
-}
+player1_name = localStorage.getItem("player1_name");
+player2_name = localStorage.getItem("player2_name");
+
+player1_score = 0;
+player2_score = 0;
+
+document.getElementById("player1_name").innerHTML = player1_name + " : ";
+document.getElementById("player2_name").innerHTML = player2_name + " : ";
+
+document.getElementById("player1_score").innerHTML = player1_score;
+document.getElementById("player2_score").innerHTML = player2_score;
+
+document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+document.getElementById("player_question").innerHTML = "Answer Turn - " + player2_name;
 
 function send() {
-number1 = document.getElementById("number1").value;
-number2 = document.getElementById("number2").value; 
-actual_answer = parseInt(number1) * parseInt(number2);
+    get_word = document.getElementById("word").value;
+    word = get_word.toLowerCase();
+    console.log("Word In Lowercase = " + word);
+
+    charAt1 = word.charAt(1);
+    console.log(charAt1);
+
+    lenght_divide_2 = Math.floor(word.length/2);
+    charAt2 = word.charAt(lenght_divide_2);
+    console.log(charAt2);
+
+    lenght_minus_1 = word.length - 1;
+    charAt3 = word.charAt(lenght_minus_1);
+    console.log(charAt3);
+
+    remove_charAt1 = word.replace(charAt1, "_");
+    remove_charAt2 = remove_charAt1.replace(charAt2, "_");
+    remove_charAt3 = remove_charAt2.replace(charAt3, "_");
+    console.log(remove_charAt3);
+
+    question_number = "<h4 id='word_display'> Q. " + remove_charAt3 + "</h4>";
+    input_box = "<br>Answer : <input type='text' id='input_check_box'>";
+    check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
+    row = question_number + input_box + check_button;
+    document.getElementById("output").innerHTML = row;
+    document.getElementById("word").value = "";
+}
+
+
+question_turn = "player1";
+answer_turn = "player2";
+
+
+function check() {
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("Answer In Lower Case - " + answer);
+    if(answer == word) {
+        if(answer_turn == "player1") {
+            player1_score = player1_score +1;
+            document.getElementById("player1_score").innerHTML = player1_score;
+        }
+        else {
+            player2_score = player2_score +1;
+            document.getElementById("player_question").innerHTML = player2_score;
+        }
+    }
+    if(question_turn == "player1") {
+        question_turn = "player2";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player2_name;
+    }
+    else {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
+    }
+
+    if(answer_turn == "player1") {
+        answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+    }
+    else {
+        answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn - " + player1_name;
+    }
+
+    document.getElementById("output").innerHTML = "";
 }
