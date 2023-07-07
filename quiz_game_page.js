4b158d958aebbf3eef9b454aad8dd15629044719
cody8764 <111713735+cody8764@users.noreply.github.com>
2023-07-07 09:11:27 -0400

    hope this is final

--- quiz_game_page.js
+++ quiz_game_page.js
@@ -17,8 +17,8 @@ function send() {
     number1 = document.getElementById("number1").value;
     number2 = document.getElementById("number2").value;
     actual_answer = parseInt(number1) * parseInt(number2);
-
-    question_number = "<h4>" + number1 + "X" + number2 + "</h4>";
+    localStorage.setItem("actual_answer", actual_answer);
+    question_number = "<h4>" + number1 + " X " + number2 + "</h4>";
     input_box = "<br>Answer : <input type='text' id='input_check_box'>";
     check_button = "<br><br><button class='btn btn-info' onclick='check()'>Check</button>";
     row = question_number + input_box + check_button ;
@@ -33,15 +33,16 @@ answer_turn = "player2";
 
 
 function check() {
+    actual_answer = localStorage.getItem("actual_answer");
     get_answer = document.getElementById("input_check_box").value;
     if(get_answer == actual_answer) {
         if(answer_turn == "player1") {
             player1_score = player1_score +1;
-            document.getElementById("player1_score").innerHTML = update_player1_score;
+            document.getElementById("player1_score").innerHTML = player1_score;
         }
         else {
             player2_score = player2_score +1;
-            document.getElementById("player_question").innerHTML = update_player2_score;
+            document.getElementById("player_question").innerHTML = player2_score;
         }
     }
     if(question_turn == "player1") {
